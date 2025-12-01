fe6b0daf2b65c9999bfa818c84cb20ae0ab467b5
vanmarkic <drag.markovic@gmail.com>
2025-12-01 17:37:36 +0100

    feat: update CompromisWrapper to accept children and integrate into compromis.astro layout

--- apps/web/src/components/CompromisWrapper.tsx
+++ apps/web/src/components/CompromisWrapper.tsx
@@ -1,14 +1,17 @@
 import { AdminSidebar } from './AdminSidebar';
 import { Providers } from './Providers';
-import { CompromisContent } from './CompromisContent';
 
-export function CompromisWrapper() {
+interface CompromisWrapperProps {
+  children: React.ReactNode;
+}
+
+export function CompromisWrapper({ children }: CompromisWrapperProps) {
   return (
     <Providers>
       <div className="min-h-screen bg-gradient-to-br from-magenta/5 to-butter-yellow/5 flex flex-col md:flex-row">
         <AdminSidebar currentPage="compromis" />
         <div className="flex-1 pt-14 md:pt-0 overflow-auto">
-          <CompromisContent />
+          {children}
         </div>
       </div>
     </Providers>
